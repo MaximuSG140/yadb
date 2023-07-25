bf7ae17343458eb9128ad0d8a28eb4c72215eda2
d.stolpovskikh <danil.stolpovskikh@gmail.com>
2023-07-26 01:12:45 +0700

    Добавил енам для кодов ошибок + структуру подобной мета информации

---
+++ src/base/meta/error_codes.h
@@ -0,0 +1,13 @@
+#ifndef YADB_ERROR_CODES_H
+#define YADB_ERROR_CODES_H
+
+namespace base{
+    namespace meta{
+        enum class ErrorCodes{
+            // format - error_code: description
+            1 = InvalidKeyword
+        };
+    }
+}
+
+#endif //YADB_ERROR_CODES_H
